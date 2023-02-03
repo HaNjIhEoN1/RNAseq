92df450a51771a7c75a300aa3ee0f801c2d69980
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-02-03 16:13:13 +0900

    Update 3. In R.R

--- 3. In R.R
+++ 3. In R.R
@@ -1,3 +1,13 @@
+library(dplyr) # data wrangling
+library(ggplot2) # plotting
+library(DESeq2) # rna-seq
+library(edgeR) # rna-seq
+library(tximport) # importing kalisto transcript counts to geneLevels
+library(readr) # Fast readr of files.
+library(rhdf5) # read/convert kalisto output files.  
 
-asdasdasdsad
-asdasd
+#If tximport and rhdf5 are not installed you can install them now.
+if (!requireNamespace("BiocManager", quietly = TRUE))
+install.packages("BiocManager")
+BiocManager::install("tximport")
+BiocManager::install("rhdf5")
