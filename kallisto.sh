14be1b030893484d9f4567ae7aa0cce986656cf8
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-02-03 15:18:20 +0900

    Create kallisto.sh

---
+++ kallisto.sh
@@ -0,0 +1,8 @@
+fasterq-dump -3 'sra_run_id'  
+  # parallel fasterq-dump -3 :::: 'srarunidlist.txt'
+kallisto index -i 'ref.idx' 'ref.fasta' # ref.fasta = reference genome
+mkdir kallisto_quantification
+for i in $(cat srarunidlist.txt)
+  do kallisto quant -i 'ref.idx' -o kallisto_quantification/ fastq/${i}_1.fastq fastq/${i}_2.fastq
+done
+zcat 'ref.cdna.all.fa.gz' | grep '>'| awk '{FS= " "}BEGIN{ print "TXNAME,GENEID"};{print substr($1,2) "," substr($4,6)};' > 'ref.cdna.csv'
