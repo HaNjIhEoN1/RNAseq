578d8c766416047cdd933e2ffb5256fd77b82e60
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-02-13 15:02:15 +0900

    Update 2. In Linux.sh

--- 2. In Linux.sh
+++ 2. In Linux.sh
@@ -5,4 +5,7 @@ mkdir kallisto_quantification
 for i in $(cat srarunidlist.txt)
   do kallisto quant -i 'ref.idx' -o kallisto_quantification/ fastq/${i}_1.fastq fastq/${i}_2.fastq
 done
+
+
+ # creating ref.cdna.csv 
 zcat 'ref.cdna.all.fa.gz' | grep '>'| awk '{FS= " "}BEGIN{ print "TXNAME,GENEID"};{print substr($1,2) "," substr($4,6)};' > 'ref.cdna.csv'
