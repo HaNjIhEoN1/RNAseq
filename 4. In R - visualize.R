3856bdc214f60b465a1b3c564b904895929be324
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-03-08 13:49:48 +0900

    Update 4. In R - visualize.R

--- 4. In R - visualize.R
+++ 4. In R - visualize.R
@@ -1,1 +1,23 @@
+#Differntial expression analysis
+dds <- DESeq(dds)
+res <- results(dds)
 
+#specified the coefficient or contrast 
+##case 1
+res <- results(dds, name="$factor_$factor1_vs_$factor2")
+##case 2
+res <- results(dds, contrast=c('$factor', '$factor1', '$factor2'))
+
+#Expoloring and exporting reults
+##MA-plot
+plotMA(res,ylim=c(-2,2))
+
+##Plot counts
+plotCounts(dds, gene = '$genename', intgroup='$factor')
+
+##Heatmap
+ntd <- normTransform(dds)
+library('pheatmap')
+select <- order(rowMeans(counts(dds,normalized=TRUE)),decreasing=TRUE)[1:20]
+df <- as.data.frame(colData(dds)[,c('$factor1','$factor2')])
+pheatmap(assay(ntd)[select,],cluster_rows=FALSE, show_rownames=FALSE,cluster_cols=FALSE, annotation_col=df)
