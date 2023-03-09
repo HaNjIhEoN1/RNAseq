9dc26f7631e387e4bb0601e0f97aac8192688768
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-03-09 22:05:00 +0900

    Update 2. In Linux.sh

--- 2. In Linux.sh
+++ 2. In Linux.sh
@@ -18,7 +18,10 @@ done
 ## calculate estimate framgent length, sd
 awk 'BEGIN { t=0.0;sq=0.0; n=0;} ;NR%4==2 {n++;L=length($0);t+=L;sq+=L*L;}END{m=t/n;printf("total %d avg=%f stddev=%f\n",n,m,sq/n-m*m);}'  *.fastq
 
-
+##
+for i in $(cat srarunidlist.txt)
+  do kallisto quant -i 'ref.idx' -o kallisto_quantification/${i}/ --single -l 200 -s 20 fastq/${i}.fastq
+done
 
  # creating ref.cdna.csv 
 zcat 'ref.cdna.all.fa.gz' | grep '>'| awk '{FS= " "}BEGIN{ print "TXNAME,GENEID"};{print substr($1,2) "," substr($4,6)};' > 'ref.cdna.csv'
