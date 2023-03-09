27056df61e90730ed1076c6150f2a39aa15be142
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-03-09 19:21:56 +0900

    Update 2. In Linux.sh

--- 2. In Linux.sh
+++ 2. In Linux.sh
@@ -7,9 +7,18 @@ for i in $(cat srarunidlist.txt)  #for making each run id's directory
   do mkdir ${i}
 done
 
+#case - paired end
+
 for i in $(cat srarunidlist.txt)
   do kallisto quant -i 'ref.idx' -o kallisto_quantification/${i}/ fastq/${i}_1.fastq fastq/${i}_2.fastq
 done
+
+#case - single end
+
+## calculate estimate framgent length, sd
+awk 'BEGIN { t=0.0;sq=0.0; n=0;} ;NR%4==2 {n++;L=length($0);t+=L;sq+=L*L;}END{m=t/n;printf("total %d avg=%f stddev=%f\n",n,m,sq/n-m*m);}'  *.fastq
+
+
 
  # creating ref.cdna.csv 
 zcat 'ref.cdna.all.fa.gz' | grep '>'| awk '{FS= " "}BEGIN{ print "TXNAME,GENEID"};{print substr($1,2) "," substr($4,6)};' > 'ref.cdna.csv'
