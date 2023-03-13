f98f83abdebf6d3bf866817f5161b72b3b1e7124
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-03-13 13:26:19 +0900

    Update 4. In R - visualize.R

--- 4. In R - visualize.R
+++ 4. In R - visualize.R
@@ -21,3 +21,11 @@ library('pheatmap')
 select <- order(rowMeans(counts(dds,normalized=TRUE)),decreasing=TRUE)[1:20]
 df <- as.data.frame(colData(dds)[,c('$factor1','$factor2')])
 pheatmap(assay(ntd)[select,],cluster_rows=FALSE, show_rownames=TRUE,cluster_cols=FALSE, annotation_col=df)
+
+vsd <- vst(dds, blind = FALSE)
+library('RColorBrewer')
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$Sample_Type)
+colnames(sampleDistMatrix)  <- NULL
+pheatmap::pheatmap(sampleDistMatrix,clustering_distance_rows = sampleDists, clustering_distance_cols = sampleDists)
