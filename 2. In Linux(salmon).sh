68352f206caa1ccd58fc5c46dad090738f50e783
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-03-13 10:22:33 +0900

    Update 2. In Linux(salmon).sh

--- 2. In Linux(salmon).sh
+++ 2. In Linux(salmon).sh
@@ -1,1 +1,23 @@
+fasterq-dump -3 'sra_run_id'  
+  # parallel fasterq-dump -3 :::: 'srarunidlist.txt'
+kallisto index -i 'ref.idx' 'ref.fasta' # ref.fasta = reference genome
+mkdir salmon
 
+cd salmon
+
+for i in $(cat srarunidlist.txt)  #for making each run id's directory
+  do mkdir ${i}
+done
+
+#making idx
+salmon index -t 'ref.fa.gz' -i ref_index
+
+# paired end
+for i in $(cat srarunidlist.txt);
+do
+salmon quant -i ref_index -l A \
+         -1 fastq/${i}_1.fastq.gz \
+         -2 fastq/${i}_2.fastq.gz \
+         -p 8 --validateMappings -o salmon/${i}
+done
+    # -p option for thread
