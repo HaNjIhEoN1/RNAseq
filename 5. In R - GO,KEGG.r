a34aa1907257448c9f2872a3c50896e7059f8e82
HaNjIhEoN1 <122258679+HaNjIhEoN1@users.noreply.github.com>
2023-04-10 10:58:46 +0900

    Update 5. In R - GO,KEGG.r

--- 5. In R - GO,KEGG.r
+++ 5. In R - GO,KEGG.r
@@ -1,11 +1,47 @@
-BiocManager::install('biomaRt')
-library(biomaRt)
-ensembl = useMart(biomart = "plants_mart",host = "https://plants.ensembl.org",dataset="osativa_eg_gene")
-get_go <- getBM(attributes = c("ensembl_gene_id","go_id"), mart = ensembl)
-get_go <- get_go[get_go$go_id != '',]
-geneID2GO <- by(get_go$go_id, get_go$ensembl_gene_id,function(x) as.character(x))
-all.genes <- sort(unique(as.character(get_go$ensembl_gene_id)))
+# kegg
 
+setwd("C:/Users/jtige/OneDrive/R")
+a <- read.csv('rt/cdddedger.csv')
+a$gene
 
+library(riceidconverter)
 
+msu <- list()
+for(i in a$gene){
+  b <- RiceIDConvert(i, 'RAP', toType = 'MSU')
+  msu <- append(msu,b)
+}
+
+repeat{
+  msu['RAP'] = NULL
+  if(length(msu['RAP'])==0) break
+}
+
+capture.output(msu, file = "my_list.txt")
+
+BiocManager::install("clusterProfiler", version = "3.16", force = TRUE)
+
+# KEGG in R
+library(clusterProfiler)
+search_kegg_organism('dosa', by='kegg_code')
+rice <- search_kegg_organism('Oryza sativa japonica', by = 'scientific_name')
+dim(rice)
+head(rice)
+genelist = a$gene
+R.utils::setOption("clusterProfiler.download.method","auto")
+
+mkk2 <- enrichKEGG(genelist, organism = 'dosa', keyType = 'kegg' ,pvalueCutoff = 0.05)
+head(mkk2)
+browseKEGG(mkk2, "dosa00480")
+
+library(DOSE)
+library(enrichplot)
+
+dotplot(mkk2, showCategory = 30, font.size = 10 )
+
+b <- read.csv('rt/asddedger.csv')
+b$gene
+genelistb = b$gene
+mkk2b <- enrichKEGG(genelistb, organism = 'dosa', keyType = 'kegg' ,pvalueCutoff = 0.05)
+dotplot(mkk2b, showCategory = 30, font.size = 10 )
 
